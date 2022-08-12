c0b1e1176e8afdfb0434957c56a6aa1c334c647f
alfajri23 <123190088@student.upnyk.ac.id>
2022-08-12 13:17:24 +0700

    change absen

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -17,36 +17,51 @@ class AbsensiController extends Controller
             session()->put('jam_max',$max);
         }
         
-        //dd($request->all());
         $hasil_tot = $request->tot/$max*50;
-        //dd(round($hasil_tot));
         $hasil_jam = 0;
 
         $akhir = substr($request->jam,3);
         $akhir = ((int)$akhir);
         $point= 0;
 
-        if($request->jam[1] == '8'){
-            if($akhir > 0 && $akhir < 10){
-                $point = 40;
-            }elseif($akhir >= 10 && $akhir < 20){
-                $point = 30;
-            }elseif($akhir >= 20 && $akhir < 30){
-                $point = 20;
-            }elseif($akhir >= 30){
-                $point = 10;
-            }elseif($akhir == 0){
-                $point = 50;
-            }
-        }else{
-            if($akhir <= 59 && $akhir > 50){
-                $point = 50;
-            }elseif($akhir <= 50 && $akhir >=40){
-                $point = 60;
-            }elseif($akhir <= 40 && $akhir >=30){
+        // if($request->jam[1] == '8'){
+        //     if($akhir > 0 && $akhir < 10){
+        //         $point = 40;
+        //     }elseif($akhir >= 10 && $akhir < 20){
+        //         $point = 30;
+        //     }elseif($akhir >= 20 && $akhir < 30){
+        //         $point = 20;
+        //     }elseif($akhir >= 30){
+        //         $point = 10;
+        //     }elseif($akhir == 0){
+        //         $point = 50;
+        //     }
+        // }else{
+        //     if($akhir <= 59 && $akhir > 50){
+        //         $point = 50;
+        //     }elseif($akhir <= 50 && $akhir >=40){
+        //         $point = 60;
+        //     }elseif($akhir <= 40 && $akhir >=30){
+        //         $point = 70;
+        //     }elseif($akhir <= 30 && $akhir >=20){
+        //         $point = 80;
+        //     }
+        // }
+
+        if($request->jam[1] == '7'){
+            if($akhir >= 0 && $akhir < 10){
                 $point = 70;
-            }elseif($akhir <= 30 && $akhir >=20){
-                $point = 80;
+            }elseif($akhir >= 10 && $akhir < 20){
+                $point = 60;
+            }elseif($akhir >= 20 && $akhir < 30){
+                $point = 50;
+            }elseif($akhir >= 30 && $akhir < 40){
+                $point = 40;
+            }elseif($akhir >= 40 && $akhir < 50){
+                $point = 30;
+            }
+            elseif($akhir >= 50){
+                $point = 20;
             }
         }
 
